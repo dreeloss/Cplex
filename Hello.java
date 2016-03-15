6bec2dadbd085c3a52a929d342924242e9612d15
amadou.barry <amadou.barry@acc.Com>
2016-03-15 11:22:37 +0100

    Change-Id: Ib82984e27bf3611d645884d21313ecf1e02544c5

--- Hello.java
+++ Hello.java
@@ -3,8 +3,5 @@ public class Hello {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
         System.out.println("Testing ");
-        System.out.println("ok ");
-        
-        
     }
 }
