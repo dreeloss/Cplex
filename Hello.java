04f4a96bc3e0c352b25f4636ce0f4faecf3d6100
amadou.barry <amadou.barry@acc.Com>
2016-03-15 14:54:07 +0100

    Change-Id: I1ecbfb0ef29a03a37afa44d68d4ae5cd0ef529a7

--- Hello.java
+++ Hello.java
@@ -4,9 +4,7 @@ public class Hello {
         System.out.println("Hello, World");
         System.out.println("Testing ");
         System.out.println("New one ");
-        
-        oki
-       
+ 
     }
 }
 
